4f077bd7a364585451f1ec452a707328630a9ac7
Ian de Jesus Barragam <ian_bp@hotmail.com>
2024-01-26 01:11:39 -0600

    First commit

---
+++ Unit Test Swift Course/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unit Test Swift Course
+//
+//  Created by Jesus Barragan  on 26/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Unit Test Swift CourseUITests/Unit_Test_Swift_CourseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Unit_Test_Swift_CourseUITestsLaunchTests.swift
+//  Unit Test Swift CourseUITests
+//
+//  Created by Jesus Barragan  on 26/01/24.
+//
+
+import XCTest
+
+final class Unit_Test_Swift_CourseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
