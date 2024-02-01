3836442797f39115c8a39d5f33c80e17ed54c3e6
Ian de Jesus Barragam <ian_bp@hotmail.com>
2024-02-01 11:53:52 -0600

    Add test

--- Unit Test Swift Course/xcTest/Calculator.swift
+++ Unit Test Swift Course/xcTest/Calculator.swift
@@ -15,8 +15,23 @@ class Calculator {
     }
     // Funcion que resta
     func substract(numbers: Int...) -> Int {
-        return numbers.reduce(into: 0, { $0 -= $1 })
+        return numbers.dropFirst().reduce(into: numbers.first ?? 0, { $0 -= $1 })
     }
+    // Funcion que divide
+    func divide(n1: Int, n2: Int) throws -> Int{
+        if(n2 == 0) {
+            throw SomeError.ZeroError
+        }
+        return n1 / n2
+    }
+    
+    func divideNoError(n1: Int, n2: Int) -> Int {
+        return n1 / n2
+    }
+}
+
+enum SomeError: Error {
+    case ZeroError
 }
 
 
--- Unit Test Swift CourseTests/xcTest/CalculatorTest.swift
+++ Unit Test Swift CourseTests/xcTest/CalculatorTest.swift
@@ -40,11 +40,73 @@ class CalculatorTest: XCTestCase {
         XCTAssertEqual(sut.add(numbers: 3, 4, 6), 13)
     }
     
+    func testCalculatorAddNumbersItsNotOkResult() throws {
+        XCTAssertNotEqual(sut.add(numbers: 3, 4, 6), 21)
+    }
+    
     /*
      MARK: Ejemplos de tests
     func testAddInputCorrectOKResult()
     func testAddInvalidInputExceptionThrown()
-     func givenWhenThen
+    func testAddAssert()
+    func givenWhenThen()
      */
     
+    //MARK: Estructura Assert
+    func testAddAssert() throws {
+        // 1. Arrange
+        let resultadoEsperado = 30
+        let resultadoActual: Int
+        // 2. Act
+        resultadoActual = sut.add(numbers: 10, 20)
+        // 3. Assert
+        XCTAssertEqual(resultadoActual, resultadoEsperado)
+    }
+    
+    //MARK: Patrón Given-When-Then
+    func testAddBDD(){
+        // 1. Given
+        let resultadoEsperado = 30
+        let resultadoActual: Int
+        // 2. When
+        resultadoActual = sut.add(numbers: 10, 20)
+        // 3. Then
+        XCTAssertEqual(resultadoActual, resultadoEsperado)
+    }
+    
+    func testAdd() throws {
+        XCTAssertEqual(30, sut.add(numbers: 10,20))
+    }
+    
+    func testAssertTypes() throws {
+        XCTAssertTrue(1 == 1)
+        XCTAssertFalse(1 == 2)
+        XCTAssertNil(nil)
+        XCTAssertNotNil(sut)
+        XCTAssertEqual(30, sut.add(numbers: 10,20))
+        XCTAssertEqual("alberto", "alberto")
+        XCTAssertGreaterThan(11, 10)
+    }
+    
+    func testSubstract() throws {
+        XCTAssertEqual(4, sut.substract(numbers: 11, 7, 0))
+    }
+    
+    func testDivide() throws {
+        XCTAssertEqual(2, try sut.divide(n1: 4, n2: 2))
+    }
+    
+    func testDivideByZero() throws {
+        XCTAssertThrowsError(try sut.divide(n1: 4, n2: 0))
+    }
+    
+    func testDisable() throws {
+        
+    }
+    
+    func testPerformanceExample() throws {
+        self.measure {
+            sut.divideNoError(n1: 8, n2: 2)
+        }
+    }
 }
